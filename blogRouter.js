672095ad49609378b0842f62805b978661542c35
Dennis Karlsson <dennis.karlsson96@outlook.com>
2020-10-05 22:35:53 +0200

    Added validation errors

--- blogRouter.js
+++ blogRouter.js
@@ -3,6 +3,10 @@ const db = require("./db")
 const router = express.Router();
 const bodyParser = require('body-parser')
 
+const TITLE_MAX_LENGTH = 25
+const CONTENT_MAX_LENGTH = 500
+const DESCRIPTION_MAX_LENGTH = 150
+
 router.use(express.static(__dirname + "/public"))
 router.use(bodyParser.urlencoded({
     extended: false
@@ -25,12 +29,12 @@ router.get("/", function (request, response) {
 //GET UPDATE blogpost
 router.get("/update-blogpost/:id", function (request, response) {
     const id = request.params.id
-    db.getBlogById(id, function(error, blogpost){
-        if(error){
+    db.getBlogById(id, function (error, blogpost) {
+        if (error) {
             //Do something
             console.log(error)
         }
-        else{
+        else {
             const model = blogpost
             response.render("update-blogpost.hbs", model)
         }
@@ -39,6 +43,9 @@ router.get("/update-blogpost/:id", function (request, response) {
 
 //CREATE new blogpost
 router.get("/create-blogpost", function (request, response) {
+    model = {
+        validationErrors: []
+    }
     response.render("create-blogpost.hbs")
 })
 
@@ -47,23 +54,56 @@ router.post("/create-blogpost", function (request, response) {
     const content = request.body.content
     const description = request.body.description
 
-    db.createNewBlogpost(title, content, description, function (error) {
-        if (error) {
-            //Do something
-            console.log(error)
-        }
-        else {
-            response.redirect("/blogs")
-        }
-    })
+    const validationErrors = []
 
+    //Validation error checks-------------------
+    if (title.length <= 0) {
+        validationErrors.push("Title is too short, please write a title!")
+    }
+    else if (title.length > TITLE_MAX_LENGTH) {
+        validationErrors.push("Title can only be 50 characters long, please shorten the title!")
+    }
+    if (content.length <= 0) {
+        validationErrors.push("Content is too short, please write some content!")
+    } 
+    else if (content.length > CONTENT_MAX_LENGTH) {
+        validationErrors.push("Content can only be 500 characters, please shorten the content!")
+    }
+    if (description.length <= 0) {
+        validationErrors.push("Description is too short, please write a description!")
+    }
+    else if (description.length > DESCRIPTION_MAX_LENGTH) {
+        validationErrors.push("Description can only be 150 charcaters, please shorten the description!")
+    }
+
+    if(validationErrors == 0) {
+        //----------------------------------
+    
+        db.createNewBlogpost(title, content, description, function (error) {
+            if (error) {
+                //Do something
+                console.log(error)
+            }
+            else {
+                response.redirect("/blogs")
+            }
+        })
+    }else{
+        model = {
+            validationErrors,
+            title,
+            content,
+            description
+        }
+        response.render("create-blogpost.hbs", model)
+    }
 })
 
 
 //GET spec Blogposts
 router.get('/:id', function (request, response) {
     const id = request.params.id
-    
+
     const blog = db.getBlogById(id, function (error, blogposts) {
         if (error) {
             //Do something
@@ -99,12 +139,12 @@ router.post("/update-blogpost/:id", function (request, response) {
     const title = request.body.title
     const content = request.body.content
     const description = request.body.description
-    db.updateBlogpost(id,title,content,description,function (error) {
+    db.updateBlogpost(id, title, content, description, function (error) {
         if (error) {
             console.log(error)
         }
         else {
-            response.redirect("/blogs/"+id)
+            response.redirect("/blogs/" + id)
         }
     })
 })
